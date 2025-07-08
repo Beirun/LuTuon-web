be0726883a33cf3b90ba6db60a913ff7276bd720
Beirun <orillobernardjay@gmail.com>
2025-07-09 01:56:46 +0800

    Add RegisterForm component

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -10,7 +10,24 @@ import {
     DialogTrigger,
 } from '@/components/ui/dialog'
 import LoginForm from './LoginForm.vue';
-import { ref } from 'vue';
+import Button from './ui/button/Button.vue';
+import Input from './ui/input/Input.vue';
+import Label from './ui/label/Label.vue';
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const screenWidth = ref(window.innerWidth)
+
+const updateWidth = () => {
+  screenWidth.value = window.innerWidth
+}
+
+onMounted(() => {
+  window.addEventListener('resize', updateWidth)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWidth)
+})
 const props = defineProps({
   open: Boolean
 })
@@ -37,48 +54,49 @@ const handleOpenChange = (openState : boolean) => {
                 </DialogTitle>
                 <DialogDescription>
                     <div class="w-full flex flex-col justify-center items-center">
-                        <div class="text-3xl sm:text-3xl md:text-4xl text-[#242629] font-bold mt-2">Account Register</div>
+                        <div class="text-3xl sm:text-3xl md:text-4xl text-foreground font-bold mt-2">Account Register</div>
                     </div>
                 </DialogDescription>
             </DialogHeader>
             <div class="px-2 sm:px-2 md:px-8 flex flex-col items-center justify-center pt-4">
                 <div class="w-full flex flex-col gap-8">
                     <div class="flex flex-col gap-1">
-                        <label for="email" class="text-base sm:text-base md:text-xl px-2 sm:px-2 md:px-4">Email</label>
-                        <input tabindex="-1" placeholder="email@example.com" id="email" type="text"
-                            class="px-2 sm:px-2 md:px-4 py-3 text-base sm:text-base md:text-lg border border-gray-500 rounded-sm sm:rounded-sm md:rounded-md focus:outline-[#F8C600]" />
+                        <Label for="email" class="text-base sm:text-base md:text-xl px-2 sm:px-2 md:px-4">Email</Label>
+                        <Input tabindex="-1" placeholder="email@example.com" id="email" type="text"
+                            class="h-14 px-2 sm:px-2 md:px-4 py-3 text-base sm:text-base md:text-lg border border-border rounded-sm sm:rounded-sm md:rounded-md focus:outline-primary focus:outline-2" />
                     </div>
                     <div class="flex flex-col gap-1">
-                        <label for="password" class="text-base sm:text-base md:text-xl px-2 sm:px-2 md:px-4">Password</label>
-                        <input tabindex="-1" placeholder="••••••••" id="password" type="password"
-                            class="px-2 sm:px-2 md:px-4 py-3 text-base sm:text-base md:text-lg border border-gray-500 rounded-sm sm:rounded-sm md:rounded-md focus:outline-[#F8C600]" />
+                        <Label for="password" class="text-base sm:text-base md:text-xl px-2 sm:px-2 md:px-4">Password</Label>
+                        <Input tabindex="-1" placeholder="••••••••" id="password" type="password"
+                            class="h-14 px-2 sm:px-2 md:px-4 py-3 text-base sm:text-base md:text-lg border border-border rounded-sm sm:rounded-sm md:rounded-md focus:outline-primary focus:outline-2" />
                     </div>
                     <div class="flex flex-col gap-1">
-                        <label for="confirmpassword" class="text-base sm:text-base md:text-xl px-2 sm:px-2 md:px-4">Confirm Password</label>
-                        <input tabindex="-1" placeholder="••••••••" id="confirmpassword" type="password"
-                            class="px-2 sm:px-2 md:px-4 py-3 text-base sm:text-base md:text-lg border border-gray-500 rounded-sm sm:rounded-sm md:rounded-md focus:outline-[#F8C600]" />
+                        <Label for="confirmpassword" class="text-base sm:text-base md:text-xl px-2 sm:px-2 md:px-4">Confirm Password</Label>
+                        <Input tabindex="-1" placeholder="••••••••" id="confirmpassword" type="password"
+                            class="h-14 px-2 sm:px-2 md:px-4 py-3 text-base sm:text-base md:text-lg border border-border rounded-sm sm:rounded-sm md:rounded-md focus:outline-primary focus:outline-2" />
                     </div>
                 </div>
-                <button
-                    class="bg-[#ECB211] text-[#242629] w-full py-3 sm:py-3 md:py-4 rounded-sm sm:rounded-sm md:rounded-md text-base sm:text-base md:text-xl font-bold active:brightness-90 hover:brightness-95 cursor-pointer hover:shadow-md hover:shadow-[#aa8700]/30 transition-all duration-300 mt-8">
+                <Button
+                    class="h-14 bg-primary text-primary-foreground w-full py-3 sm:py-3 md:py-4 rounded-sm sm:rounded-sm md:rounded-md text-base sm:text-base md:text-xl font-bold active:brightness-90 hover:brightness-95 cursor-pointer hover:shadow-md hover:shadow-[#aa8700]/30 transition-all duration-300 mt-8">
                     REGISTER
-                </button>
-                <div class="w-full py-4 flex justify-center">
-                    <span class="text-[#242629] text-sm sm:text-sm md:text-lg font-light"> Already Have an Account? </span>
+                </Button>
+                <div class="w-full py-4 flex justify-center items-center">
+                    <span class="text-foreground text-sm sm:text-sm md:text-lg font-light"> Already Have an Account? </span>
                     &nbsp;
-                            <button @click="openLoginDialog" class="text-[#C08F06] text-sm sm:text-sm md:text-lg font-light hover:underline hover:brightness-80">Login
-                                Here!</button>
+                            <Button variant="link" @click="openLoginDialog" class="px-0 text-primary brightness-85 text-sm sm:text-sm md:text-lg font-light hover:underline hover:brightness-80">Login
+                                Here!</Button>
                 </div>
                 <div class="w-full flex justify-center items-center gap-4">
-                    <div class="h-[1px] w-full bg-gray-700"></div>
+                    <div class="h-[1px] w-full bg-foreground"></div>
                     <div class="text-sm sm:text-sm md:text-base">OR</div>
-                    <div class="h-[1px] w-full bg-gray-700"></div>
+                    <div class="h-[1px] w-full bg-foreground"></div>
                 </div>
-                <button
-                    class="flex justify-center items-center gap-4 bg-[#F1F1F1] text-[#242629] w-full py-4 my-4 rounded-md text-base sm:text-base md:text-xl  active:brightness-90 hover:brightness-95 cursor-pointer hover:shadow-md hover:shadow-[#aa8700]/20 transition-all duration-300">
+                <Button
+                    variant="outline"
+                    class="h-14 flex justify-center items-center gap-4 bg-background brightness-95 text-foreground w-full my-4 rounded-md text-base sm:text-base md:text-xl  active:brightness-90 hover:brightness-95 cursor-pointer hover:shadow-md hover:shadow-primary-foreground/20 transition-all duration-300">
                     <img src="@/assets/google.png" class="size-6 sm:size-6 md:size-8" alt="">
                     REGISTER WITH GOOGLE
-                </button>
+                </Button>
             </div>
         </DialogContent>
     </Dialog>

cb9a47db0abfb0ff7e0a1699f4efcb0fb24149b6
Beirun <orillobernardjay@gmail.com>
2025-08-29 23:07:28 +0800

    feat: add api plugin

---
+++ src/plugins/api.ts
@@ -0,0 +1,59 @@
+import { useAuthStore } from "@/stores/auth"
+import { useSonnerStore } from "@/stores/sonner"
+let token: string | null = localStorage.getItem("token")
+let isRefreshing = false
+let url = import.meta.env.VITE_BASE_URL
+let refreshPromise: Promise<string | null> | null = null
+
+async function refreshAccessToken(): Promise<string | null> {
+  const auth = useAuthStore()
+  const sonner = useSonnerStore()
+
+  if (isRefreshing && refreshPromise) return refreshPromise
+  isRefreshing = true
+
+
+  refreshPromise = (async () => {
+    try {
+      const res = await fetch(url + "/accounts/refresh", {
+        method: "POST",
+        credentials: "include", // send HttpOnly cookie
+      })
+
+      if (!res.ok) throw new Error("Session Expired")
+
+      const data = await res.json()
+      localStorage.setItem("token", data.token)
+      token = data.token
+      return data.token
+    } catch (err: any) {
+      sonner.message(err.message, "Please Log-In again")
+      auth.handleTokenExpiry()
+      return null
+    } finally {
+      isRefreshing = false
+      refreshPromise = null
+    }
+  })()
+
+  return refreshPromise
+}
+
+
+export async function useFetch(input: RequestInfo, init: RequestInit = {}): Promise<Response> {
+  if (!init.headers) init.headers = {}
+  if (token) {
+    (init.headers as Record<string, string>)["Authorization"] = `Bearer ${token}`
+  }
+
+  let res = await fetch(input, init)
+  if (res.status === 401 || res.status === 403) {
+    const newToken = await refreshAccessToken()
+    if (newToken) {
+      (init.headers as Record<string, string>)["Authorization"] = `Bearer ${newToken}`
+      res = await fetch(input, init)
+    }
+  }
+
+  return res
+}

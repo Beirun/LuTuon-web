a25822872690f292c9e21dc89d1b4b7f21984b5c
Beirun <orillobernardjay@gmail.com>
2025-08-29 23:07:28 +0800

    feat: add reset store

---
+++ src/stores/reset.ts
@@ -0,0 +1,98 @@
+// stores/reset.ts
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import { useFetch } from "@/plugins/api";
+import { useSonnerStore } from "./sonner";
+import router from "@/router";
+export const useResetStore = defineStore("reset", () => {
+  const loading = ref(false);
+  const code = ref<string | null>(null);
+  const expiresAt = ref<string | null>(null);
+  const URL = import.meta.env.VITE_BASE_URL
+
+  const sonner = useSonnerStore();
+
+  async function requestReset(email: string) {
+    
+    loading.value = true;
+    try {
+      const res = await useFetch(URL+`/reset/request`, {
+        method: "POST",
+        body: JSON.stringify({ email: email }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const data = await res.json();
+      if (!res.ok) throw new Error(data.message || "Failed to request reset");
+
+      code.value = data.code;
+      expiresAt.value = data.expiresAt;
+
+      sonner.success(data.message);
+    } catch (e: any) {
+      sonner.error(e.message || "An error occurred");
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  async function verifyCode(email: string, enteredCode: string) {
+    loading.value = true;
+    try {
+      const res = await useFetch(URL+`/reset/verify`, {
+        method: "POST",
+        body: JSON.stringify({ email, code: enteredCode }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const data = await res.json();
+      if (!res.ok) throw new Error(data.message || "Invalid or expired code");
+
+      sonner.success(data.message);
+      return data.resetId;
+    } catch (e: any) {
+      sonner.error(e.message || "An error occurred");
+      return null;
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  async function resetPassword(
+  email: string,
+  enteredCode: string,
+  newPassword: string,
+) {
+  loading.value = true;
+  try {
+
+    const res = await useFetch(URL+`/reset/password`, {
+      method: "POST",
+      body: JSON.stringify({ email, code: enteredCode, newPassword }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message || "Failed to reset password");
+
+    sonner.success(data.message);
+    code.value = null;
+    expiresAt.value = null;
+    router.push("/")
+  } catch (e: any) {
+    sonner.error(e.message || "An error occurred");
+  } finally {
+    loading.value = false;
+  }
+}
+
+
+  return {
+    loading,
+    code,
+    expiresAt,
+    requestReset,
+    verifyCode,
+    resetPassword,
+  };
+});

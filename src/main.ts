42184fc801c29f1281b7fafe8a0060c9d408c293
Beirun <orillobernardjay@gmail.com>
2025-09-05 18:51:22 +0800

    chore: update main entry file

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import GoogleSignInPlugin  from 'vue3-google-login';
+
 const app = createApp(App)
+
+app.use(GoogleSignInPlugin ,{
+    clientId: import.meta.env.VITE_CLIENT_ID
+});
 
 app.use(createPinia())
 app.use(router)

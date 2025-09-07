6f1c97e832b2436502998ec9a4f2f4992959fd5d
Beirun <orillobernardjay@gmail.com>
2025-09-07 23:20:08 +0800

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig, loadEnv, type ConfigEnv, type UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
 
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    plugins: [vue(), tailwindcss()],
+    plugins: [vue(), 
+      tailwindcss(),
+      vueDevTools()
+    ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))

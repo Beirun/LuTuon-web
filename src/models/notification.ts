ada318c80a6ba5e042db81eee4470f35e231cf13
Beirun <orillobernardjay@gmail.com>
2025-09-09 18:13:52 +0800

    feat: add notification model

---
+++ src/models/notification.ts
@@ -0,0 +1,8 @@
+export type Notification = {
+    userId: string;
+    notificationId: string;
+    notifiactionTitle: string;
+    notificationMessage: string;
+    notificationStatus: string;
+    notificationDate: string
+}

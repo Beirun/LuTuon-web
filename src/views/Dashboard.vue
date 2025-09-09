fe94fb67a2cd1766c3c22079ca36add549be6a38
Beirun <orillobernardjay@gmail.com>
2025-09-09 18:19:55 +0800

    dark mode update

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -76,17 +76,17 @@ ChartJS.register(
         </div>
         <Separator class="text-[#DBDBE0] mt-5 mb-13 sm:mb-0" />
         <div class="w-full h-full flex flex-col gap-10 justify-end mt-[3vh] sm:mt-0">
-          <div class="w-full sm:h-1/2 h-[25vh] outline-1 rounded-2xl pt-5 pb-16 pl-5 pr-8">
+          <div class="w-full sm:h-1/2 h-[25vh] outline-1 dark:outline-gray-200/10 dark:bg-[#1e1e1e]/10 bg-[#e8e8e8]/10 rounded-2xl pt-5 pb-16 pl-5 pr-8">
             <p class="lg:text-2xl font-bold pb-4 px-5">Registered Users</p>
             <Line :data="lineData" :options="lineOptions" />
           </div>
           <div class="w-full lg:h-2/5 h-full flex lg:flex-row flex-col gap-10 pb-10">
 
-            <div class="sm:w-1/2 w-full sm:h-full h-[25vh] outline-1 rounded-2xl p-5 pb-12">
+            <div class="sm:w-1/2 w-full sm:h-full h-[25vh] outline-1 dark:outline-gray-200/10 dark:bg-[#1e1e1e]/10 bg-[#e8e8e8]/10 rounded-2xl p-5 pb-12">
               <p class="lg:text-2xl font-bold">Most Played Dish</p>
               <Bar :data="hBarData" :options="hBarOptions" />
             </div>
-            <div class="sm:w-1/2 w-full sm:h-full h-[25vh] outline-1 rounded-2xl p-5 pb-12">
+            <div class="sm:w-1/2 w-full sm:h-full h-[25vh] outline-1 dark:outline-gray-200/10 dark:bg-[#1e1e1e]/10 bg-[#e8e8e8]/10 rounded-2xl p-5 pb-12">
               <p class="lg:text-2xl font-bold">Weekly Players</p>
               <Bar :data="vBarData" :options="vBarOptions" />
             </div>

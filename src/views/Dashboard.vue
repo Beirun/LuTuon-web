4229a6ab3fe224bcb42a397912c704db0a534ca8
Beirun <orillobernardjay@gmail.com>
2025-09-09 18:13:51 +0800

    chore: update Dashboard view

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -73,20 +73,19 @@ ChartJS.register(
       <div class="w-7/8 h-7/8">
         <div class="flex justify-between mb-10 lg:mb-5">
           <p class="text-3xl">Welcome to dashboard, <b>Admin!</b></p>
-          <Bell />
         </div>
         <Separator class="text-[#DBDBE0] mb-10 lg:mb-0" />
         <div class="w-full h-full flex flex-col gap-10 justify-end">
-          <div class="w-full h-1/2 outline-1 rounded-2xl pt-5 pb-16 pl-5 pr-8">
+          <div class="w-full h-1/2 outline-1 dark:outline-gray-200/10 dark:bg-[#1e1e1e]/10 bg-[#e8e8e8]/10 rounded-2xl pt-5 pb-16 pl-5 pr-8">
             <p class="lg:text-2xl font-bold pb-4 px-5">Registered Users</p>
             <Line :data="lineData" :options="lineOptions" />
           </div>
           <div class="w-full lg:h-2/5 h-full flex lg:flex-row flex-col gap-10 pb-10">
-            <div class="w-full lg:w-1/2 h-full outline-1 rounded-2xl p-5 pb-12">
+            <div class="w-full lg:w-1/2 h-full outline-1 dark:outline-gray-200/10 dark:bg-[#1e1e1e]/10 bg-[#e8e8e8]/10 rounded-2xl p-5 pb-12">
               <p class="lg:text-2xl font-bold">Most Played Dish</p>
               <Bar :data="hBarData" :options="hBarOptions" />
             </div>
-            <div class="w-full lg:w-1/2 h-full outline-1 rounded-2xl p-5 pb-12">
+            <div class="w-full lg:w-1/2 h-full outline-1 dark:outline-gray-200/10 dark:bg-[#1e1e1e]/10 bg-[#e8e8e8]/10 rounded-2xl p-5 pb-12">
               <p class="lg:text-2xl font-bold">Weekly Players</p>
               <Bar :data="vBarData" :options="vBarOptions" />
             </div>
